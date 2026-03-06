f17238af2485b50db5eee3897d6636740802a71f
Carlos Rossi <carlitosrossi@gmail.com>
2026-03-06 16:56:24 -0300

    fix section heights

---
+++ practise.js
@@ -0,0 +1,12 @@
+let city = "Mendoza";
+let name = "Carlos";
+let isBilingual = true;
+
+function introduce(name, city) {
+  return "I'm " + name + " from " + city + "!";
+}
+
+console.log(introduce("Carlos", "Mendoza"));
+
+let skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+console.log(skills[0]);
